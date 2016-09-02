c608579457462e9a4705b96ca998e33d7d8aa74e
nmass17 <windward_student@school.windwardschool.org>
2016-09-02 09:06:25 -0700

    Prints Pokeball

---
+++ src/Pokeball.java
@@ -0,0 +1,20 @@
+//This Pokeball graphic can be used to in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon
+public class Pokeball {
+	public static void main(String[] args){
+		System.out.println("    @@@@@@\n  @@@@@@@@@@");
+		System.out.println(" @@@@@@@@@@@@\n@@@@@@@@@@@@@@");
+		System.out.println("||||[\"\"\"\"]||||");
+		System.out.println("||||[....]||||");
+		System.out.println("OOOOOOOOOOOOOO\n OOOOOOOOOOOO");
+		System.out.println("  OOOOOOOOOO\n    OOOOOO");
+	}
+}
+
+/*
+If you want to
+leave a note for future programmers
+in with your code
+place two slashes, like this // in front of each line of your comment
+Another alternative is to enclose your comments with
+/*, but not \*!
+*/
