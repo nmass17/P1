d1fd5c49419ad838edef1a4e1141341d9a55478f
nmass17 <windward_student@school.windwardschool.org>
2016-09-02 09:41:59 -0700

    v2
    Has answers for all 6 questions

--- src/Pokeball.java
+++ src/Pokeball.java
@@ -1,4 +1,4 @@
-//This Pokeball graphic can be used to in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon
+//#1
 public class Pokeball {
 	public static void main(String[] args){
 		System.out.println("    @@@@@@\n  @@@@@@@@@@");
@@ -10,6 +10,10 @@ public class Pokeball {
 	}
 }
 
+//#2
+//This Pokeball graphic can be used to in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon
+
+//#3
 /*
 If you want to
 leave a note for future programmers
@@ -18,3 +22,21 @@ place two slashes, like this // in front of each line of your comment
 Another alternative is to enclose your comments with
 /*, but not \*!
 */
+
+//#4
+//The class name is Organization because it references the class Organization at the beginning of the program
+
+//#5
+/*
+1. Forgot the word class between public and the class name
+2. Main is spelled mian
+3. line 3, print is capitalized when it should be lowercase
+4. line3, no semicolon at the end of the statement
+5. line 4, no quote at the end of the string
+6. no curly brace to close the first open curly brace
+
+*/
+
+//#6
+// missing syntax, incorrect spelling, capitalization, semicolon after a statement
+// quotes at the beginning and end of a string, close all parenthesis, brackets, and braces
